333e2d0024995d7b0a1fa17c7051eeefb152636a
barangievans <barangievans@gmail.com>
2024-07-15 11:44:04 +0300

    acting

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+// dodger.js
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left) || 180;
+  if (left > 0) {
+    left -= 1;
+    dodger.style.left = left + 'px';
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left) || 180;
+  if (left < 360) {
+    left += 1;
+    dodger.style.left = left + 'px';
+  }
+}
+
+module.exports = {
+  moveDodgerLeft,
+  moveDodgerRight
+};
